302a4be8ea86a35b3d82651dedbe7c50dca20a08
daniel <dejong543@gmail.com>
2021-10-25 12:04:04 +0200

    completed dbcontext & set conn string

--- webike/Models/dbcontext.cs
+++ webike/Models/dbcontext.cs
@@ -7,7 +7,15 @@ namespace webike.Models
 
     public class WebikeContext : DbContext
     {
-
+        public DbSet<Cyclist> Cyclists {get;set;}
+        public DbSet<Contact> Contacts {get;set;}
+        public DbSet<Admin> Admins{get;set;}
+        public DbSet<Event> Events {get;set;}
+        public DbSet<EventActivity> EventActivities {get;set;}
+        public DbSet<Excercise> Excercises {get;set;}
+        public DbSet<Rating> Ratings {get;set;}
+        public DbSet<Route> Routes {get;set;}
+        public DbSet<Workout> Workouts {get;set;}
         
 
 
