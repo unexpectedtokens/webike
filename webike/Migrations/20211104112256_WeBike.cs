b179627a6b5f2270485dac8015c31c71f03701d1
majd-as <85077049+majd-as@users.noreply.github.com>
2021-11-06 11:50:15 +0100

    rating

---
+++ webike/Migrations/20211104112256_WeBike.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace webike.Migrations
+{
+    public partial class WeBike : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
